3b9f5583f2240e8d76b90dbbdbfa8e905e0841b5
nhhamza <hamza.lhimer@soprasteria.com>
2020-01-17 09:08:55 +0100

    feat(comment): fix pr comments.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Header from './components/header';
 
@@ -6,24 +6,15 @@ import Home from './containers/Home';
 import About from './containers/About';
 import Users from './containers/Users';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <>
-        <Header />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/users" component={Users} />
-        </Switch>
-      </>
-    );
-  }
-}
+const App = () => (
+  <>
+    <Header />
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/users" component={Users} />
+    </Switch>
+  </>
+);
 
 export default App;
