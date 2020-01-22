05363cb9269d15b23f33fc285b2053ae4c69a612
nhhamza <nh_hamza@hotmail.fr>
2020-01-22 16:22:19 +0100

    feat(env): add ui theme.

--- src/index.jsx
+++ src/index.jsx
@@ -3,14 +3,20 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import { Provider } from 'react-redux';
+import { ThemeProvider, Typography } from '@material-ui/core';
 import { store } from './redux/store';
 
 import App from './App.jsx';
+import theme from './styles/theme';
 
 ReactDOM.render(
   <Provider store={store}>
     <Router>
-      <App />
+      <ThemeProvider theme={theme}>
+        <Typography>
+          <App />
+        </Typography>
+      </ThemeProvider>
     </Router>
   </Provider>,
   document.getElementById('app'),
