f414a3551005df9ab2c972ca0dde7de67f2c746e
nhhamza <hamza.lhimer@soprasteria.com>
2020-01-27 12:28:45 +0100

    wip2

--- src/components/dropzone/Dropzone.jsx
+++ src/components/dropzone/Dropzone.jsx
@@ -3,8 +3,28 @@ import { DropzoneArea } from 'material-ui-dropzone';
 import { makeStyles } from '@material-ui/core';
 import style from './style.scss';
 import { acceptedFiles } from '../../constants/ui.constants';
+import t from '../../constants/translates';
 
 const Dropzone = props => {
+  const getFileAddedMessage = fileName => {
+    return t.formatString(t.fileUploader.fileAddedMessage, { fileName });
+  };
+  const getFileLimitExceedMessage = filesLimit => {
+    return t.formatString(t.fileUploader.fileLimitExceedMessage, {
+      filesLimit,
+    });
+  };
+  const getFileRemovedMessage = fileName => {
+    return t.formatString(t.fileUploader.fileRemovedMessage, {
+      fileName,
+    });
+  };
+  const getDropRejectMessage = file => {
+    return t.formatString(t.fileUploader.fileRemovedMessage, {
+      fileName: file.name,
+    });
+  };
+
   return (
     <DropzoneArea
       {...props}
@@ -13,6 +33,10 @@ const Dropzone = props => {
       filesLimit={15}
       dropzoneParagraphClass={style.paragraph}
       showFileNames
+      getFileAddedMessage={getFileAddedMessage}
+      getFileLimitExceedMessage={getFileLimitExceedMessage}
+      getFileRemovedMessage={getFileRemovedMessage}
+      getDropRejectMessage={getDropRejectMessage}
     />
   );
 };
