603b89966cda0f2b42ab198243e866432dc3c0bc
JGARCIA4 <Javier.GARCIA-EXT@nexity.fr>
2020-01-31 12:15:31 +0100

    feat(card): created card , prev list of categories

--- src/components/dialog/dialog.jsx
+++ src/components/dialog/dialog.jsx
@@ -13,16 +13,14 @@ const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
 });
 
-const AlertDialogSlide = props => {
-  const {
-    open,
-    title,
-    content,
-    handleClose,
-    textButtonOk,
-    textButtonCancel,
-  } = props;
-
+const AlertDialogSlide = ({
+  open,
+  title,
+  content,
+  handleClose,
+  textButtonOk,
+  textButtonCancel,
+}) => {
   return (
     <div>
       <Dialog
